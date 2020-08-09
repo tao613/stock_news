ea85dfa02394e8364acdb988d059666fb0a58c4b
tao613 <jintao613@163.com>
2020-08-09 16:04:00 +0800

    upsert

--- news_collection.py
+++ news_collection.py
@@ -193,6 +193,9 @@ def save2ESEach(news_dict):
 						# 		}
 						# 	}
 						)
+					print('Symbol Changed. ', 
+							es_news.get(index=index_name, doc_type=doc_type, id=news_dict['URL'], ignore=[404, 400]).['_source']
+						)
 				else:
 					print('Symbol no change.')
 			else:
